38b15ca26d254439b32b9eedea730df6d32cd89c
Gzar81 <116944846+Gzar81@users.noreply.github.com>
2023-05-03 01:53:12 +0200

    Creado componente únete

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UneteComponent } from './components/unete/unete.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UneteComponent
   ],
   imports: [
     BrowserModule,
