e04225405d266aa2807dd3cc308ceb880e5fc251
Lorenzo López Rodríguez <lorenlr14@gmail.com>
2023-05-06 12:16:16 +0200

    Creación del componente teacher-card

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
-import { TeacherCardComponent } from './components/teacher-card/teacher-card.component';
+import { TeacherComponent } from './components/teacher/teacher.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
-  { path: 'teachers', component: TeacherCardComponent },
+  { path: 'teachers', component: TeacherComponent },
   { path: '**', component: HomeComponent }
 ];
 
