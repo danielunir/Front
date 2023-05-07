07c73af1a2f5312f9f98d77e54f36d6ddbcb43b5
Jesus Martin Blanco <jesusmartinb64@gmail.com>
2023-05-07 11:46:50 +0200

    Actualización del componente registro

--- src/app/components/usuarios/registro/registro.component.ts
+++ src/app/components/usuarios/registro/registro.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators, AbstractControl } from '@angular/forms';
 
 @Component({
   selector: 'app-registro',
@@ -10,6 +11,61 @@ export class RegistroComponent {
   disabled = true;
   contador = 0;
   contadorMax = 255;
+
+  formRegister: FormGroup;
+
+  constructor() {
+
+    this.formRegister = new FormGroup({
+      rol: new FormControl("",[]),
+      username: new FormControl("",[
+        Validators.required,
+        Validators.minLength(3)
+      ]),
+      email: new FormControl("",[
+        Validators.required,
+        Validators.pattern(/^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/)
+      ]),
+      password: new FormControl("",[
+        Validators.required,
+        Validators.pattern(/^(?=(.*[a-zA-Z].*){2,})(?=.*\d.*)(?=.*\W.*)[a-zA-Z0-9\S]{8,15}$/)
+      ]),
+      confirmPassword: new FormControl("",[
+        Validators.required
+      ]),
+      nombre: new FormControl("", [
+        Validators.required,
+        Validators.minLength(3),
+        Validators.maxLength(45)
+      ]),
+      apellidos: new FormControl("", [
+        Validators.required,
+        Validators.maxLength(45)
+      ]),
+      direccion: new FormControl("", [
+        Validators.required,
+        Validators.maxLength(100)
+      ]),
+      ciudad: new FormControl("",[
+        Validators.required,
+        Validators.maxLength(45)
+      ]),
+      codigo_postal: new FormControl("",[
+        Validators.required,
+        Validators.minLength(5),
+        Validators.maxLength(5)
+      ]),
+      telefono: new FormControl("",[
+        Validators.required,
+        Validators.minLength(9),
+        Validators.maxLength(12)
+      ]),
+      fecha_nacimiento: new FormControl("",[]),
+
+    },[
+      this.checkPassword
+    ])
+  }
 
   continue($event: any) {
 
@@ -26,6 +82,28 @@ export class RegistroComponent {
       $event.preventDefault();
 
     }
+  }
+
+  checkPassword(pFormValue: AbstractControl) {
+
+    const password: string = pFormValue.get('password')?.value;
+    const confirmPassword: string = pFormValue.get('confirmPassword')?.value;
+
+    if(password !== confirmPassword) {
+      return { 'checkpassword': true }
+    }
+    return null;
+  }
+
+  checkControl(pControlName: string, pError: string): boolean {
+    if(this.formRegister.get(pControlName)?.hasError(pError) && this.formRegister.get(pControlName)?.touched) {
+      return true;
+    }
+    return false;
+  }
+
+  getDataForm() {
+
   }
 
 }
