483af711b02258cfd718637777cfda410c95b515
Ainoa Dorado Iglesias <ainoadorado@gmail.com>
2023-05-09 15:11:05 +0200

    Modificación ts, html y css del componente login

--- src/app/components/usuarios/login/login.component.ts
+++ src/app/components/usuarios/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+
+  formLogin: FormGroup;
+
+  constructor(
+  ) {
+    this.formLogin = new FormGroup({
+      email: new FormControl(),
+      password: new FormControl()
+    });
+  }
+
+  onSubmit() {
+  }
 
 }
