76f62a52b70d046d8d688a5615d17b297bb76cc7
Ainoa Dorado Iglesias <ainoadorado@gmail.com>
2023-05-09 17:06:59 +0200

    Modificación login.ts

--- src/app/components/usuarios/login/login.component.ts
+++ src/app/components/usuarios/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
+import { UsuariosService } from 'src/app/services/usuarios.service';
 
 @Component({
   selector: 'app-login',
@@ -11,6 +13,8 @@ export class LoginComponent {
   formLogin: FormGroup;
 
   constructor(
+    private usuariosService: UsuariosService,
+    private router: Router
   ) {
     this.formLogin = new FormGroup({
       email: new FormControl(),
@@ -18,7 +22,16 @@ export class LoginComponent {
     });
   }
 
-  onSubmit() {
+  async onSubmit() {
+    const response = await this.usuariosService.login(this.formLogin.value);
+
+    if (response.fatal) {
+      return alert(response.fatal);
+    }
+
+    localStorage.setItem('token_login', response.token);
+    this.usuariosService.changeLogin(true);
+    this.router.navigate(['/users/profile']);
   }
 
 }
