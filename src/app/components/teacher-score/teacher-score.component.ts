59d9e69689c1be2352e55ce061f3813c9a8877e1
Jesus Martin Blanco <jesusmartinb64@gmail.com>
2023-05-19 19:13:50 +0200

    Se crea el componente teacherScore y se maqueta el componente graficInfo

---
+++ src/app/components/teacher-score/teacher-score.component.ts
@@ -0,0 +1,37 @@
+import { Component } from '@angular/core';
+import { PuntuacionService } from 'src/app/services/puntuacion.service';
+import { TeachersService } from 'src/app/services/teachers.service';
+
+@Component({
+  selector: 'app-teacher-score',
+  templateUrl: './teacher-score.component.html',
+  styleUrls: ['./teacher-score.component.css']
+})
+export class TeacherScoreComponent {
+
+  teachers_list: any;
+
+  pageHome: boolean = true;
+  constructor(
+    private teachersService: TeachersService,
+    private puntuacionService: PuntuacionService
+  ) { }
+
+  async ngOnInit() {
+    this.teachers_list = [];
+
+    this.bestScore();
+  }
+
+  async bestScore(): Promise<void> {
+    try {
+      const response = await this.puntuacionService.getBestScore();
+      console.log(response);
+      this.teachers_list = response;
+      }
+      catch (error) {
+      alert('No hay profesores disponibles en la BBDD');
+    }
+  }
+}
+
