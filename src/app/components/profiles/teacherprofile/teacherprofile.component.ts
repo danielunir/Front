18d655755038ea37f8efab2cbb928eafac6ed43a
Ainoa Dorado Iglesias <ainoadorado@gmail.com>
2023-05-23 16:26:35 +0200

    Modificacion ramas.service

--- src/app/components/profiles/teacherprofile/teacherprofile.component.ts
+++ src/app/components/profiles/teacherprofile/teacherprofile.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { RamasService } from 'src/app/services/ramas.service';
 import { TeachersService } from 'src/app/services/teachers.service';
 
 @Component({
@@ -10,14 +11,21 @@ export class TeacherprofileComponent {
 
   logados: boolean = true;
   data: any = {}
+  alumnos: any = []
 
   constructor(
     private teachersService: TeachersService,
+    private ramasService: RamasService
   ) {
   }
 
   async ngOnInit() {
     const userId = localStorage.getItem('user_id')
     this.data = await this.teachersService.getByUserId(userId);
+    const teacherId = this.data.id
+    console.log(teacherId)
+    this.alumnos = await this.ramasService.getByUserId(userId);
+    console.log(this.alumnos)
+
   }
 }
