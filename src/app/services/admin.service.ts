77a2fc9d6d1c03f4fc7ef164c8057e592ea2f90b
Ainoa Dorado Iglesias <ainoadorado@gmail.com>
2023-05-24 11:01:59 +0200

    Creación servicio admin

---
+++ src/app/services/admin.service.ts
@@ -0,0 +1,33 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { firstValueFrom } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AdminService {
+
+  private baseUrl: string;
+
+  constructor(private httpClient: HttpClient) {
+    this.baseUrl = 'https://teachers-groupb.herokuapp.com/api/admin';
+  }
+
+
+
+  getByUserId(userId: any) {
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Authorization': localStorage.getItem('token_login')!
+      })
+    }
+
+    return firstValueFrom(
+      this.httpClient.get<any>(`${this.baseUrl}/${userId}`, httpOptions)
+    )
+  }
+
+
+}
+
+
