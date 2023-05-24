eacd9f801537b11a55ad735c923ac808e1c7b984
Lorenzo López Rodríguez <lorenlr14@gmail.com>
2023-05-24 10:40:01 +0200

    Actualización del perfil profesor, con los datos de la petición

--- src/app/components/profiles/teacherprofile/teacherprofile.component.ts
+++ src/app/components/profiles/teacherprofile/teacherprofile.component.ts
@@ -11,26 +11,26 @@ import { TeachersService } from 'src/app/services/teachers.service';
 })
 export class TeacherprofileComponent implements OnInit {
   teacher: any;
-  
+
   data: any = {}
   alumnos: any = []
-  
+
   logados: boolean = true;
 
   constructor(
     private teacherService: TeachersService,
     private activateRoute: ActivatedRoute,
-     private ramasService: RamasService
+    private ramasService: RamasService
   ) { }
 
   async ngOnInit() {
     const userId = localStorage.getItem('user_id')
-    this.data = await this.teachersService.getByUserId(userId);
+    this.data = await this.teacherService.getByUserId(userId);
     const teacherId = this.data.id
     console.log(teacherId)
     this.alumnos = await this.ramasService.getByUserId(userId);
     console.log(this.alumnos)
-    
+
     this.activateRoute.params.subscribe(async (params: any) => {
       let currentId: number = params.teacherId;
       let response: any = await this.teacherService.getByUserId(currentId);
