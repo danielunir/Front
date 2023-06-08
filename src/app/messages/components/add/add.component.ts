ced7f35b84c933216d44f751acdd7d21124b15cf
Jesus Martin Blanco <jesusmartinb64@gmail.com>
2023-06-08 10:06:55 +0200

    Se introduce mensaje personalizado de envio de mensaje en el componente add componente

--- src/app/messages/components/add/add.component.ts
+++ src/app/messages/components/add/add.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { MessageService } from 'src/app/services/message.service';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { AlumnosService } from 'src/app/services/alumnos.service';
+import { TeachersService } from 'src/app/services/teachers.service';
+import { RamasService } from 'src/app/services/ramas.service';
 
 @Component({
   selector: 'app-add',
@@ -16,13 +19,17 @@ export class AddComponent implements OnInit {
 
   remitente: any;
   destinatario: any;
-
+  alumno: any;
+  profesor: any;
+  role: string | null= '';
 
 
   constructor(
     private messageService: MessageService,
     private activatedRoute: ActivatedRoute,
-    private router: Router
+    private router: Router,
+    private ramasService: RamasService,
+    private alumnosService: AlumnosService
     ) {
       this.title = 'Enviar mensaje';
 
@@ -36,13 +43,35 @@ export class AddComponent implements OnInit {
 
     }
 
-    ngOnInit(): void {
+    async ngOnInit() {
 
       const remitenteId: number = this.activatedRoute.snapshot.params['remitenteId'];
       const destinatarioId: number = this.activatedRoute.snapshot.params['destinatarioId'];
 
       this.remitente = remitenteId;
       this.destinatario = destinatarioId;
+      console.log(remitenteId)
+      console.log(destinatarioId)
+
+      try {
+        this.role = localStorage.getItem('user_role');
+        if (this.role === 'profesor') {
+          const alumnos = await this.ramasService.getByUserId(remitenteId);
+          if (alumnos) {
+            let alumno = alumnos.filter((alumno: any) =>
+               alumno.alumno.usuario_id === Number(this.destinatario));
+            this.alumno = alumno;
+          }
+        } else if (this.role === 'alumno') {
+          const profesores = await this.alumnosService.getAllTeachers(remitenteId);
+          if (profesores) {
+            let profesor = profesores.filter((profesor: any) => profesor.datos_per.usuario_id === Number(this.destinatario));
+            this.profesor = profesor;
+          }
+        }
+      } catch (error) {
+        console.log(error);
+      }
 
   }
 
